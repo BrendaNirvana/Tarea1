dc997c9e926d6850bd0db147583b43ae9dc20854
BrendaNirvana <133972427+BrendaNirvana@users.noreply.github.com>
2023-06-21 23:31:34 +0000

    archivo donde no se usó hoja de jupyter

--- archivo_solo_programacion.py
+++ archivo_solo_programacion.py
@@ -1,4 +1,3 @@
 #Esto es un comentario
 #vamos a calcular el area de un circulo: pi*r**2
-print(3.1416 * 4 ** 2)
-
+print(3.1416 * 4 ** 2) 
---
+++ prueba_1era_tarea.py
@@ -0,0 +1,6 @@
+#esto es una prueba para ver si sale la primera tarea
+#Necesitamos sacar la siguiente operacion matematica
+
+print(3**(1/2))
+print(5*4-3**(1/3))
+
