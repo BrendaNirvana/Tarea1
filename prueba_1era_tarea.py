b9d26753da762dccca707ebaa3251031082b2d68
BrendaNirvana <133972427+BrendaNirvana@users.noreply.github.com>
2023-06-21 23:49:58 +0000

    prueba de la primera tarea -sí salió :D-

--- prueba_1era_tarea.py
+++ prueba_1era_tarea.py
@@ -1,6 +1,6 @@
 #esto es una prueba para ver si sale la primera tarea
 #Necesitamos sacar la siguiente operacion matematica
 
-print(3**(1/2))
-print(5*4-3**(1/3))
 
+print(4 * (3**2)-2* (5*4-3**3))
+#Recuerda que los parentesis no son multiplicacion y hay que anitar el simbolo de mult. 
