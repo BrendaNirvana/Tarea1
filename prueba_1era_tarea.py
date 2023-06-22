8038e9115159f356e7bc5345b62fc296137892e3
BrendaNirvana <133972427+BrendaNirvana@users.noreply.github.com>
2023-06-21 18:01:46 -0600

    Delete prueba_1era_tarea.py

--- prueba_1era_tarea.py
+++
@@ -1,6 +0,0 @@
-#esto es una prueba para ver si sale la primera tarea
-#Necesitamos sacar la siguiente operacion matematica
-
-
-print(4 * (3**2)-2* (5*4-3**3))
-#Recuerda que los parentesis no son multiplicacion y hay que anitar el simbolo de mult. 
